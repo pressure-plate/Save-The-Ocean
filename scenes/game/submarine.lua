b68f53c1373cc9ab5e55ba05656ec6855490e30b
Simone <not.simone.work@outlook.com>
2020-08-15 01:59:40 +0200

    Better Submarine movement function

--- scenes/game/submarine.lua
+++ scenes/game/submarine.lua
@@ -18,37 +18,52 @@ local submarineDir = "assets/submarine/"
 -- functions
 -- ----------------------------------------------------------------------------
 
+
+
 local function moveSubmarine( self, event )
 
 	local transRot -- var to hold the rotation transition reference
-	local rotDeg = 15 -- rotation degree
-	local rotTime = 1000 -- rotation time
+	local minRaiseRotation = 5 -- rotation degree
+	local rotDeg = 35 -- rotation degree
+	local rotTime = 500 -- rotation time
 
 	-- start of touch
     if ( event.phase == "began" ) then
         
 		 -- Set touch focus on the submarine (this means that the submarine object will "own" the touch event throughout its duration)
 		display.currentStage:setFocus( self )
+		
 
-		-- rotate submarine
+		-- rise listener of submarine
+		local function raiseListener( obj )
+			submarineIsRising = true
+			self:setLinearVelocity( 0, -submarineRisingSpeed )	
+		end
+
+		-- rotate submarine, and on complete call the raise listener
 		transition.cancel( transRot )
+		transRot = transition.to( self, {rotation = -minRaiseRotation, time = rotTime/2, onComplete = raiseListener} )
+		
+		-- complete the raise rotation to the max degree
 		transRot = transition.to( self, {rotation = -rotDeg, time = rotTime} )
 
-		-- rise of submarine
-		submarineIsRising = true
-		self:setLinearVelocity( 0, -submarineRisingSpeed )
 		
 	elseif ( event.phase == "ended" or event.phase == "cancelled" ) then
 		-- Release touch focus on the ship
 		display.currentStage:setFocus( nil )
 
+		-- fall listener of submarine
+		local function fallListener( obj )
+			submarineIsRising = false
+			self:setLinearVelocity( 0, submarineRisingSpeed )	
+		end
+
 		-- change rotation
 		transition.cancel( transRot )
-		transRot = transition.to( self, {rotation = rotDeg, time = rotTime} )
+		transRot = transition.to( self, {rotation = minRaiseRotation, time = rotTime/2, onComplete = fallListener} )
 	
-		-- fall of submarine
-		submarineIsRising = false
-		self:setLinearVelocity( 0, submarineRisingSpeed )	
+		-- complete the fall rotation to the max degree	
+		transRot = transition.to( self, {rotation = rotDeg, time = rotTime} )
 	end
 	
     return true  -- Prevents touch propagation to underlying objects
@@ -56,18 +71,20 @@ end
 
 local function onEnterFrame( self, event ) 
 
+	local rotTime = 500 -- rotation time
+
 	-- check bounds of submarine rotation -------------------------------------
 	if (submarineIsRising == true and self.y < 70) then
 		submarineIsRising = false
 		self:setLinearVelocity( 0, 0 )
 		transition.cancel( self )
-		transition.to( self, {rotation = 0, time = 150} )
+		transition.to( self, {rotation = 0, time = rotTime} )
 	
 	elseif (submarineIsRising == false and self.y > display.contentHeight-70) then
 		submarineIsRising = true
 		self:setLinearVelocity( 0, 0 )
 		transition.cancel( self )
-		transition.to( self, {rotation = 0, time = 150} )
+		transition.to( self, {rotation = 0, time = rotTime} )
 	end
 end
 
@@ -82,7 +99,7 @@ function M.init( group )
     -- init vars
     M.submarine = nil
     submarineIsRising = false
-    submarineRisingSpeed = 600
+    submarineRisingSpeed = 400
 
 	-- load submarine skin
 	local submarinePaint = {
