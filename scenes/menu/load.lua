ab4f720cfd3db81bd5cf4696eac33a1923f12a61
Mattia Fedrigo <mattia.fedrigo@hotmail.it>
2020-08-20 01:22:34 +0200

    fixes at load module

--- scenes/menu/load.lua
+++ scenes/menu/load.lua
@@ -2,19 +2,21 @@ local M = {}
 
 local composer = require( "composer" )
 
+-- load submarine module
+local subMod = require( "scenes.game.submarine" )
+
 -- initialize variables -------------------------------------------------------
 
 -- assets directory
-local uiDir = "assets/submarine/" -- user interface assets di
+local submarineDir = "assets/submarine/" -- user interface assets di
 
-local submarinesCount = 5
+local submarinesCount = 6
 local submarineScaleFactor = 0.8
 local submarineWidth = 512/(display.contentWidth/(16*80))
 local submarineHeight = 265/(display.contentHeight/(9*80))
 
 local group
 local submarines = {}
-M.selectedSubmarine = 1
 
 local originX = display.contentCenterX - display.contentWidth/4
 local originY = display.contentCenterY - display.contentHeight/4
@@ -24,7 +26,7 @@ local rowCount = 2
 
 
 local function selectItem( event )
-    M.selectedSubmarine = event.target.submarineId
+    subMod.submarineSkin = event.target.submarineId
     print( "You touched the object ".. event.target.submarineId )
 end
 
@@ -40,17 +42,17 @@ end
 function M.loadSubmarines()
 
     -- load submarines as grid
-    local i = 0
+    local i = 1
     -- loop row
     for row = 1, rowCount do
         -- loop col
         for col = 0, colCount - 1 do
             -- generate submarine
-            local submarine = display.newImage(group, uiDir .. (i+1) .. ".png") -- set title
+            local submarine = display.newImage(group, submarineDir .. (i) .. ".png") -- set title
             submarine:scale( submarineScaleFactor, submarineScaleFactor )
             submarine.x = originX + submarine.width*submarineScaleFactor * col * 1.2
             submarine.y = originY + submarine.height*submarineScaleFactor * row * 1.2
-            submarine.submarineId = i+1
+            submarine.submarineId = i
             submarine:addEventListener( "tap", selectItem ) -- tap listener
             table.insert(submarines, submarine) -- save the submarine to remove it later
 
